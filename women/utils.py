92879efc3dc4e547065a63aad067df12d01b8129
Алексей Коган (рабочий) <alekseikogan@yandex.ru>
2024-01-23 18:05:44 +0300

    добавил пагинацию

--- women/utils.py
+++ women/utils.py
@@ -2,7 +2,6 @@ from django.db.models import Count
 
 from .models import Category
 
-
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Обратная связь", 'url_name': 'contact'}]
@@ -14,13 +13,11 @@ class DataMixin:
     def get_user_context(self, **kwargs):
         context = kwargs
         cats = Category.objects.annotate(Count('women'))
-
         user_menu = menu.copy()
         if not self.request.user.is_authenticated:
             user_menu.pop(1)
 
         context['menu'] = user_menu
-
         context['cats'] = cats
         if 'cat_selected' not in context:
             context['cat_selected'] = 0
